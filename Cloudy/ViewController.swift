a135a74ac8613e655bcd3ff2d690fad757b9671c
Maximilian Harazin <maximilian.harazin@gmail.com>
2020-01-01 11:41:33 +0100

    Initial Commit

---
+++ Cloudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cloudy
+//
+//  Created by Maximilián Harazin on 01/01/2020.
+//  Copyright © 2020 Maximilián Harazin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
