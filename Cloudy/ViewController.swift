717764b49a52c02d4b74363d3c9c448831156664
Maximilian Harazin <maximilian.harazin@gmail.com>
2020-01-01 15:28:45 +0100

    Add IBOutlet for storyboard elements

--- Cloudy/ViewController.swift
+++ Cloudy/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Temperature View
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    // Forecast View
+    @IBOutlet weak var forecast1Label: UILabel!
+    @IBOutlet weak var forecast1Temp: UILabel!
+    @IBOutlet weak var forecast1Image: UIImageView!
+    
+    @IBOutlet weak var forecast2Label: UILabel!
+    @IBOutlet weak var forecast2Temp: UILabel!
+    @IBOutlet weak var forecast2Image: UIImageView!
+    
+    @IBOutlet weak var forecast3Label: UILabel!
+    @IBOutlet weak var forecast3Temp: UILabel!
+    @IBOutlet weak var forecast3Image: UIImageView!
+    
+    @IBOutlet weak var forecast4Label: UILabel!
+    @IBOutlet weak var forecast4Temp: UILabel!
+    @IBOutlet weak var forecast4Image: UIImageView!
+    
+    @IBOutlet weak var forecast5Label: UILabel!
+    @IBOutlet weak var forecast5Temp: UILabel!
+    @IBOutlet weak var forecast5Image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
